11429d833bf2c4c6b91bfae9683423a8dbd3eaff
LoloSD <lolosuarez071@gmail.com>
2026-06-25 02:06:37 +0200

    Configurado Firebase Messaging Service Worker

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,12 +2,13 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.5/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyB_vBfGEGsv0If66XM7oAXUQjXUtv5B614",
+  authDomain: "agafona-app.firebaseapp.com",
+  projectId: "agafona-app",
+  storageBucket: "agafona-app.firebasestorage.app",
+  messagingSenderId: "952287808321",
+  appId: "1:952287808321:web:6fc690fb51b6b5b80e2fc0",
+  measurementId: "G-FE2E1T9834"
 });
 
 const messaging = firebase.messaging();
@@ -16,10 +17,11 @@ messaging.onBackgroundMessage((payload) => {
   console.log("Notificación recibida en segundo plano:", payload);
 
   const titulo = payload.notification?.title || "AGAFONA";
+
   const opciones = {
     body: payload.notification?.body || "Tienes una nueva notificación",
-    icon: "/assets/logo-agafona.png",
-    badge: "/assets/logo-agafona.png"
+    icon: "/agafona-app/assets/logo-agafona.png",
+    badge: "/agafona-app/assets/logo-agafona.png"
   };
 
   self.registration.showNotification(titulo, opciones);
